b791a38bb47869edebbbcfaf3e2013c1802da71a
Starlight220 <yotamshlomi@gmail.com>
2020-08-09 00:06:56 +0300

    fix carriage return not being identified as line separator

--- assembler/macros.h
+++ assembler/macros.h
@@ -7,7 +7,7 @@
 #define EQ(c, n) ((c) == (n))
 #define EOS(c) EQ(c,'\0')
 #define eqEOF(c) EQ(c, EOF)
-#define EOL(c) EQ(c,'\n')
+#define EOL(c) (EQ(c,'\n') || EQ(c, '\r'))
 
 #define END(c) (EOS(c) || eqEOF(c) || EOL(c))
 #define WHT(c) (EQ(c, ' ') || EQ(c, '\t'))
