8b9875f6e15b528d49ce4c2e4cf7cff3a4be6b46
nadavhad <68025197+nadavhad@users.noreply.github.com>
2020-08-21 09:05:28 +0300

    finished documenting constants.h

--- constants.h
+++ constants.h
@@ -11,15 +11,15 @@
 #define MAX_PARAMS 2
 /* Maximum amount of tokens from any line*/
 #define MAX_TOKENS 4
-
+/* Maximum length of a file name */
 #define MAX_FILE_NAME 100
-
+/*Maximum positive number that can fit into of 24 bits (the size of a "word")*/
 #define MAX_24_BIT_WORD 8388607
-
+/*Minimum negative number that can fit into of 24 bits (the size of a "word")*/
 #define MIN_24_BIT_WORD -8388608
-
+/*Maximum positive number that can fit into of 21 bits (the size of a "word" excluding A R E bits)*/
 #define MAX_21_BIT_WORD 1048575
-
+/*Minimum negative number that can fit into of 21 bits (the size of a "word" excluding A R E bits)*/
 #define MIN_21_BIT_WORD -1048576
 
 /* Number of Operations available in .as syntax*/
